ac73bf6be73ffe1b846eb979c1116e4457230df1
audgks5551 <audgks5551@daum.net>
2022-02-11 19:30:44 +0900

    순수 자바로만 멤버, 주문 구현

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,22 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    // db 역할
+    private static Map<Long, Member> store = new HashMap<>();
+    // 원래는 ConcurrentHashMap<> 사용해야함
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
