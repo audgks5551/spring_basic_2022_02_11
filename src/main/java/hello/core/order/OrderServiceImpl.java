9f6ad7e298a1b53c48e8bc9cd922739158ee2bfc
audgks5551 <audgks5551@daum.net>
2022-02-13 10:20:33 +0900

    롬복 추가하고 생성자주입을 롬복에 맡김

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,10 +3,12 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
@@ -17,11 +19,12 @@ public class OrderServiceImpl implements OrderService{
     // 의존 관계가 확실하게
     private final DiscountPolicy discountPolicy;
 
-    @Autowired // 생성자가 하나일 경우 생략 가능
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+    // @RequiredArgsConstructor와 같음
+//    @Autowired // 생성자가 하나일 경우 생략 가능
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
 
     @Override
