d09c20f9a9fe274823052c8ee1d0b215d3df25af
audgks5551 <audgks5551@daum.net>
2022-02-13 17:07:56 +0900

    실수를 줄이기위한 어노테이션 만들기

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,5 +1,6 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 //@Qualifier("mainDiscountPolicy") // 의존 주입할 때 두개 이상의 구현체가 있으면 지정가능
+//@MainDiscountPolicy // 위와 같은 효과 나타남(어노테이션을 만든 이유는 컴파일시 문자를 잘못 작성하여도 컴파일 오류가 안나기 때문)
 @Primary
 public class RateDiscountPolicy implements DiscountPolicy{
 
