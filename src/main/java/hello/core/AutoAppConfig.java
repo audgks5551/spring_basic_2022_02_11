9a10a6a12c080065ae4c1812a34f7c02176ce9a3
audgks5551 <audgks5551@daum.net>
2022-02-13 09:02:42 +0900

    component scan의 exclude include filter만들기

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = {"hello.core"}, // 기본값
+        basePackageClasses = AutoAppConfig.class, // 기본값
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
